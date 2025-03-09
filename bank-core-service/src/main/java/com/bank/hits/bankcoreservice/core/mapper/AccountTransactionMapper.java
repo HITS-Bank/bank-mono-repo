ceed587799e3c651054b8bedbd58b98fb3072ca3
Пупов Иван <ivan.a.pupov@gazprombank.ru>
2025-03-09 15:06:35 +0300

    правки маппера транзакций

--- bank-core-service/src/main/java/com/bank/hits/bankcoreservice/core/mapper/AccountTransactionMapper.java
+++ bank-core-service/src/main/java/com/bank/hits/bankcoreservice/core/mapper/AccountTransactionMapper.java
@@ -3,9 +3,14 @@ package com.bank.hits.bankcoreservice.core.mapper;
 import org.mapstruct.Mapper;
 import com.bank.hits.bankcoreservice.api.dto.AccountTransactionDto;
 import com.bank.hits.bankcoreservice.core.entity.AccountTransaction;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface AccountTransactionMapper {
+
+    @Mapping(source = "transactionId", target = "id")
+    @Mapping(source = "transactionType", target = "type")
+    @Mapping(source = "transactionDate", target = "executedAt")
     AccountTransactionDto map(final AccountTransaction tx);
 
     AccountTransaction map(final AccountTransactionDto dto);
