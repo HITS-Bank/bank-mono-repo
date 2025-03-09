04ace9ff59f1f5534bd3bc2c28259aefc7c2291a
Пупов Иван <ivan.a.pupov@gazprombank.ru>
2025-03-09 18:11:37 +0300

    поправил запос

--- bank-core-service/src/main/java/com/bank/hits/bankcoreservice/api/dto/TopUpRequest.java
+++ bank-core-service/src/main/java/com/bank/hits/bankcoreservice/api/dto/TopUpRequest.java
@@ -1,9 +1,12 @@
 package com.bank.hits.bankcoreservice.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TopUpRequest {
+    @JsonProperty("accountNumber")
     private final String accountNumber;
+    @JsonProperty("amount")
     private final String amount;
 }
