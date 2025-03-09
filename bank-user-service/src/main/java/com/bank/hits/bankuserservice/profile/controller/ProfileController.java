44a2836db8d212c071f1e9afb878713820a2d62d
Серегин Матвей <matvey.s.seregin@gazprombank.ru>
2025-03-09 14:43:57 +0700

    Пофиксил роутинг для юзер сервиса

--- bank-user-service/src/main/java/com/bank/hits/bankuserservice/profile/controller/ProfileController.java
+++ bank-user-service/src/main/java/com/bank/hits/bankuserservice/profile/controller/ProfileController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/employee/profile")
+@RequestMapping("/users/employee/profile")
 @RequiredArgsConstructor
 public class ProfileController {
 
